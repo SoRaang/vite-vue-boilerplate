7b14b1e7d3af1d4b2bf7c02d15761b296e1c99ba
SoRaang <dsc0320@hotmail.com>
2022-12-13 14:39:31 +0900

    Update

--- src/components/HeadPrimary.vue
+++ src/components/HeadPrimary.vue
@@ -1,12 +1,14 @@
 <template>
     <header id="headPrimary" class="flex justify-center items-center sticky top-0 h-16 bg-white/[.05] backdrop-blur-lg">
-        <div class="container">
-            <h1 id="mainLogo">
+        <div class="container flex justify-between items-center">
+            <h1 id="mainLogo" class="hover:text-emerald-500">
                 <a href="#">Devoist</a>
             </h1>
 
-            <nav id="navPrimary">
-                <button type="button" class="nav-item">업무일정표</button>
+            <nav id="navPrimary" class="flex items-center gap-2">
+                <button type="button" class="nav-item">프로젝트별 일정</button>
+                <button type="button" class="nav-item">개인별 업무일정표</button>
+                <button type="button" class="nav-item">자료실</button>
             </nav>
         </div>
     </header>
@@ -17,10 +19,20 @@
 </script>
 
 <style lang="scss" scoped>
+    #mainLogo {
+        font-family: 'Fredericka the Great', cursive;
+        font-size: 1.8rem;
+        font-weight: 900;
+    }
+
     #headPrimary {
 
     }
+
     .nav-item {
-        @apply py-1 px-3 rounded bg-white/[.05] hover:bg-blue-300/[.3]; // Tailwind CSS의 유틸리티 스타일을 하나의 클래스 등에 포함시키는 방법
+        @apply py-1 px-3 rounded bg-white/[.05] shadow-lg text-[.6rem]; // Tailwind CSS의 유틸리티 스타일을 하나의 클래스 등에 포함시키는 방법
+        @apply hover:bg-blue-300/[.3];
+        @apply focus:bg-blue-300/[.15] focus:outline focus:outline-offset-2 focus:outline-1 focus:outline-emerald-500;
+        @apply active:bg-blue-300/[.5];
     }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,6 @@
 <template>
     <main id="contentView">
         <div class="container mx-auto my-4">
-            뭔가 심상치 않은 일이 생긴거야
-            어둠 속에 숨어있는 적의 무리
-            아름다운 녹색의 별 지구를 침범하려 해
-
-            <img src="src/assets/images/img.gif" alt="">
-
             <button @click="numb ++">
                 버튼 : {{ numb }}
             </button>
