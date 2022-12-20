a688b4fed2afb56d57d3b584bee2434ed4af2e9d
SoRaang <dsc0320@hotmail.com>
2022-12-20 17:41:28 +0900

    1

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,10 @@ import Main from '@/views/Main.vue'
 
 const routes = [
     { path: '/', name: 'Main', component: Main },
-    { path: '/projects', name: 'Projects', component: () => import('@/views/Projects.vue') }, // Lazy-Loading을 위한 JavaScript 청크 분리
-    { path: '/workflow', name: 'Workflow', component: () => import('@/views/Workflow.vue') },
-    { path: '/storage', name: 'Storage', component: () => import('@/views/Storage.vue') },
+    { path: '/projects', name: 'Projects', component: () => import( /* webpackChunkName: 'Projects' */ '@/views/Projects.vue') }, // Lazy-Loading을 위한 JavaScript 청크 분리
+    { path: '/workflow', name: 'Workflow', component: () => import( /* webpackChunkName: 'Workflow' */ '@/views/Workflow.vue') },
+    { path: '/storage', name: 'Storage', component: () => import( /* webpackChunkName: 'Storage' */ '@/views/Storage.vue') },
+    { path: '/details/:id', component: () => { /* webpackChunkName: 'ItemDetail' */ '@/views/ItemDetail.vue' } }
 ]
 
 const router = createRouter({
---
+++ src/views/ItemDetail.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>여기는 {{ $route.params.id }}</h1>
+    </div>
+
+    <!-- Template Ends -->
+</template>
+
+<script setup>
+    /* Logic Ends */
+</script>
+
+<style lang="scss" scoped>
+    // Stylesheet Ends
+</style>
