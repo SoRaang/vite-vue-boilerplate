dfa000c29d5c2f1ba24290030c6796887bf401f7
SoRaang <dsc0320@hotmail.com>
2023-01-02 17:54:55 +0900

    123

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const routes = [
     { path: '/projects', name: 'Projects', component: () => import( /* webpackChunkName: 'Projects' */ '@/views/Projects.vue') }, // Lazy-Loading을 위한 JavaScript 청크 분리
     { path: '/workflow', name: 'Workflow', component: () => import( /* webpackChunkName: 'Workflow' */ '@/views/Workflow.vue') },
     { path: '/storage', name: 'Storage', component: () => import( /* webpackChunkName: 'Storage' */ '@/views/Storage.vue') },
-    { path: '/details/:id', component: () => { /* webpackChunkName: 'ItemDetail' */ '@/views/ItemDetail.vue' } }
+    { path: '/details/:id', component: () => import( /* webpackChunkName: 'ItemDetail' */ '@/views/ItemDetail.vue' ) }
 ]
 
 const router = createRouter({
--- src/views/ItemDetail.vue
+++ src/views/ItemDetail.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>여기는 {{ $route.params.id }}</h1>
+        <h1>여기는 <span class="text-emerald-500 font-black">{{ $route.params.id }}</span> 이라고 입력된 페이지임</h1>
     </div>
 
     <!-- Template Ends -->
