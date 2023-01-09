f0e23fd711cdfefe3b1d93aa1f6f19848239f01b
SoRaang <dsc0320@hotmail.com>
2023-01-09 11:40:18 +0900

    13123

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,7 @@
         <HeadPrimary></HeadPrimary>
 
         <main id="contentView">
-            <div class="container mx-auto my-4 px-2">
-                <router-view></router-view>
-            </div>
-
-            <div @click="aniThis" id="helloworld">
-                김헬로월드
-            </div>
+            <router-view></router-view>
         </main>
 
         <FootPrimary></FootPrimary>
@@ -17,16 +11,8 @@
 </template> <!-- Template Ends -->
 
 <script setup>
-    import anime from 'animejs'
     import HeadPrimary from './components/HeadPrimary.vue'
     import FootPrimary from './components/FootPrimary.vue'
-
-    const aniThis = () => anime({
-        targets: '#helloworld',
-        translateX: 250,
-        rotate: '1turn',
-        duration: 800
-    })
 </script> <!-- Logic Ends -->
 
 <style lang="scss" scoped>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,8 @@
 <template>
-    <div id="mainPage" class="flex flex-col gap-4">
+    <div id="mainPage" class="flex flex-col gap-4 container mx-auto my-4 px-2">
         <h1>Main</h1>
+
+        <p style="color: --color-text-highlight">김하이라이트</p>
     </div>
 </template> <!-- Template Ends -->
 
