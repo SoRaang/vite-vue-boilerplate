c61ac11bc5e4651da2b2a85f7022dea81f696745
junsjins <junsjins@nate.com>
2023-01-12 10:03:19 +0900

    11

---
+++ src/views/Portfolio.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container mt-20 mx-auto">
+        <h1>여기는 구축사례 입니당</h1>
+        
+    </div>
+</template> <!-- Template Ends -->
+
+<script setup>
+
+</script> <!-- Logic Ends -->
+
+<style lang="scss">
+
+</style> <!-- Stylesheet Ends -->
