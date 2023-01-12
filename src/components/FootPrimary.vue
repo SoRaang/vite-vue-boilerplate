0875e4314d3107d1184118196349c5e3b99c97d6
SoRaang <dsc0320@hotmail.com>
2023-01-12 10:01:44 +0900

    123

--- src/components/FootPrimary.vue
+++ src/components/FootPrimary.vue
@@ -2,6 +2,21 @@
     <footer id="footPrimary">
         <div class="container mx-auto">
             <span class="text-[.6rem] opacity-50">© {{ currentYear }} SoRaang</span>
+
+            <div class="flex justify-end gap-2 m-5">
+                <div class="color-chip" style="background-color: rgb(var(--clr-id-main))"></div>
+                <div class="color-chip" style="background-color: rgb(var(--clr-id-sub))"></div>
+                <div class="color-chip" style="background-color: rgb(var(--clr-id-01))"></div>
+                <div class="color-chip" style="background-color: rgb(var(--clr-id-02))"></div>
+                <div class="color-chip" style="background-color: rgb(var(--clr-inter-neutral))"></div>
+                <div class="color-chip" style="background-color: rgb(var(--clr-inter-agree))"></div>
+                <div class="color-chip" style="background-color: rgb(var(--clr-inter-warn))"></div>
+                <div class="color-chip" style="background-color: rgb(var(--clr-inter-error))"></div>
+                <div class="color-chip" style="background-color: rgb(var(--clr-inter-disable))"></div>
+                <div class="color-chip" style="background-image: linear-gradient(to top left, rgb(16, 27, 110), rgb(0, 78, 255))"></div>
+                <div class="color-chip" style="background-image: linear-gradient(to top left, rgb(66, 0, 255), rgb(0, 255, 108))"></div>
+                <div class="color-chip" style="background-image: linear-gradient(to top left, rgb(40, 65, 255), rgb(202, 255, 247))"></div>
+            </div>
         </div>
     </footer>
 </template> <!-- Template Ends -->
@@ -13,5 +28,11 @@
 <style lang="scss">
     #footPrimary {
         @apply mt-auto px-2 py-4;
+    }
+
+    .color-chip {
+        width: 2rem;
+        height: 2rem;
+        border-radius: .15rem;
     }
 </style> <!-- Stylesheet Ends -->
---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+
+</template> <!-- Template Ends -->
+
+<script setup>
+
+</script> <!-- Logic Ends -->
+
+<style lang="scss" scoped>
+
+</style> <!-- Stylesheet Ends -->
