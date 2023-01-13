dfd49f6d62775d7199de07cec8740270fa9d7458
joahJS <joahjoah0@gmail.com>
2023-01-13 11:52:25 +0900

    230113

--- src/views/CsNotice.vue
+++ src/views/CsNotice.vue
@@ -1,9 +1,22 @@
 <template>
     <div id="tempSubPageHero">
-
+        <div id="divHeroSubline">
+            <ul id="ulHeroSub">
+                <li>공지사항</li>
+                <li>자료실</li>
+                <li>소식</li>
+            </ul>
+            <ul id="ulBreadCrumbs">
+                <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"><path fill="none" d="M0 0h24v24H0z"/><path d="M20 20a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-9H1l10.327-9.388a1 1 0 0 1 1.346 0L23 11h-3v9z" fill="rgba(255,255,255,1)"/></svg>
+                </li>
+                <li>고객센터</li>
+                <li>공지사항</li>
+            </ul>
+        </div>
     </div>
-    <section id="Notice" class="cs-container">
-        <div class="total-search-line">
+    <section id="secNotice" class="cs-container">
+        <div id="divSearchLine">
             <select data-totalsearch-select>
                 <option value="cd-total">전체</option>
                 <option value="cd-title">제목</option>
@@ -11,55 +24,117 @@
             </select>
             <input data-totalsearch-input type="text">
         </div>
-        <div id="NoticeTexts">
-            <div class="notice-table-title">
+        <div id="divNotice">
+            <div class="table-title">
                 <div>번호</div>
                 <div>제목</div>
                 <div>등록일</div>
             </div>
-            <div class="notice-table-text">
-                <div>3</div>
-                <div>소나무정보기술 공휴일 휴무안내</div>
-                <div>등록일</div>
+            <div id="divTextLines">
+                <div class="table-text">
+                    <div>4</div>
+                    <div class="board-titles-div">소나무정보기술 공휴일 휴무안내</div>
+                    <div>2023.01.13</div>
+                </div>
+                <div class="table-text">
+                    <div>3</div>
+                    <div class="board-titles-div">소나무정보기술 공휴일 휴무안내</div>
+                    <div>2023.01.12</div>
+                </div>
+                <div class="table-text">
+                    <div>2</div>
+                    <div class="board-titles-div">소나무정보기술 공휴일 휴무안내</div>
+                    <div>2023.01.11</div>
+                </div>
+                <div class="table-text">
+                    <div>1</div>
+                    <div class="board-titles-div">소나무정보기술 공휴일 휴무안내</div>
+                    <div>2023.01.10</div>
+                </div>
             </div>
+            
         </div>
     </section>
     
 </template>
 
 <script setup>
-    //import SubPageHero from '@/components/SubPageHero.vue'
+    
 </script>
 
 <style lang="scss" scoped>
 
-    #Notice{
-        @apply flex flex-col;
+ 
+
+    #secNotice{
+        @apply flex flex-col pt-10;
     }
     .cs-container{
         @apply container max-w-screen-xl mx-auto;
     }
 
+/* subPageHero */
+
     #tempSubPageHero{
-        @apply relative w-full h-96;
+        @apply relative w-full h-96 flex items-end;
+        background:url(@/assets/images/subHeroBack.png) no-repeat;
+        
     }
 
-    #NoticeTexts{
-        @apply w-full py-6 border-black border-t-2;
-        > * {
+    #divHeroSubline{
+        @apply flex container max-w-screen-xl mx-auto;
+        > ul{
             @apply flex;
         }
     }
 
-    .notice-table-title{
+    #ulHeroSub{
+        > li{
+            @apply w-48 h-10 text-base flex justify-center items-center hover:opacity-100 hover:text-black cursor-pointer opacity-50;
+            // background:rgba(255,255,255, .5);
+            background-color: rgb(var(--clr-base));
+            font-family:'NS-Neo';
+        }
+    }
+
+    #ulBreadCrumbs{
+        @apply items-center ml-auto drop-shadow-lg;
+        > li{
+            @apply text-white text-base;
+            & + li:before{
+                content:'>';
+                @apply pr-2 pl-2;
+            }
+        }
+    }
+
+/* divNotice */    
+
+    #divNotice{
+        @apply w-full border-black border-t-2 px-1.5;
         
     }
 
-    .notice-table-text{
-
+    .table-title{
+        @apply grid text-center text-base text-black border-b border-black py-2;
+        grid-template-columns:6rem 1fr 10rem;        
     }
 
-    .total-search-line{
+    .table-text{
+        @apply grid text-center cursor-pointer;
+        grid-template-columns:6rem 1fr 10rem; 
+        border-bottom:1px solid rgba(0,0,0, .15);
+        > div{
+            @apply text-black py-3;
+            font-size:var(--fnt-title-sm);
+        }
+    }
+
+    .board-titles-div{
+        @apply text-start px-2;
+    }
+
+    #divSearchLine{
         @apply border-gray-100 w-full h-8 mb-4 flex justify-end;
         > * {
             @apply h-full;
